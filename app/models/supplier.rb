6007a2473403241ea22c8a4de2575d11a0ab1545
Chreees79 <christelle.gevaert32@gmail.com>
2022-12-05 18:04:46 +0100

    add Supplier scaffold

---
+++ app/models/supplier.rb
@@ -0,0 +1,8 @@
+class Supplier < ApplicationRecord
+  belongs_to :user
+  validates :alias, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :zipcode, presence: true
+  
+end
