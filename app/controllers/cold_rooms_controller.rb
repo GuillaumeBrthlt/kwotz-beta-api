2e59a31bbdf67c2f1fdb0f7f55b77b9c97ee8edd
Chreees79 <christelle.gevaert32@gmail.com>
2022-12-06 12:11:02 +0100

    scaffolding cold_room

---
+++ app/controllers/cold_rooms_controller.rb
@@ -0,0 +1,50 @@
+class ColdRoomsController < ApplicationController
+  before_action :set_cold_room, only: %i[ show update destroy ]
+  # GET /cold_rooms
+  def index
+    @cold_rooms = ColdRoom.all
+
+    render json: @cold_rooms
+  end
+
+  # GET /cold_rooms/1
+  def show
+    render json: @cold_room
+  end
+
+  # POST /cold_rooms
+  def create
+    @cold_room = ColdRoom.new(cold_room_params)
+
+    if @cold_room.save
+      render json: @cold_room, status: :created, location: @cold_room
+    else
+      render json: @cold_room.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /cold_rooms/1
+  def update
+    if @cold_room.update(cold_room_params)
+      render json: @cold_room
+    else
+      render json: @cold_room.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /cold_rooms/1
+  def destroy
+    @cold_room.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cold_room
+      @cold_room = ColdRoom.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def cold_room_params
+      params.require(:cold_room).permit(:temperature, :condensing_unit, :prod_outside, :refrigerant_type, :length, :width, :height, :volume, :product_types, :entries_frequency, :entries_quantity, :heat_sources_power, :heat_sources, :comment)
+    end
+end
