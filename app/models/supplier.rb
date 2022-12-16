0d22272567a5b22bdfb48292c06a08680610bb56
GuillaumeBrthlt <bertholetguillaume@gmail.com>
2022-12-16 09:43:48 +0100

    dependent destroy on contacts and expiration token at 1 week

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,6 +1,6 @@
 class Supplier < ApplicationRecord
   belongs_to :user
-  has_many :supplier_contacts
+  has_many :supplier_contacts, dependent: :destroy
   validates :alias, presence: true
   validates :address, presence: true
   validates :city, presence: true
